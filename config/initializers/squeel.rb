6d6cc94e6fbadda2513c8a191c56d7b5e3178327
Riley Dallas <rldaggie@gmail.com>
2012-06-10 10:09:15 -0500

    Added squeel initializer

--- config/initializers/squeel.rb
+++ config/initializers/squeel.rb
@@ -2,7 +2,7 @@ Squeel.configure do |config|
   # To load hash extensions (to allow for AND (&), OR (|), and NOT (-) against
   # hashes of conditions):
   #
-  # config.load_core_extensions :hash
+  config.load_core_extensions :hash
 
   # To load symbol extensions (for a subset of the old MetaWhere functionality,
   # via ARel predicate methods on Symbols: :name.matches, etc):
